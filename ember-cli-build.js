ca9d60283a4c3e4dd966c50a84aa95a7111b42d8
Denis Toledo <contato@denistoledo.com.br>
2021-01-04 09:37:58 -0300

    first commit

---
+++ ember-cli-build.js
@@ -0,0 +1,28 @@
+'use strict';
+
+const EmberApp = require('ember-cli/lib/broccoli/ember-app');
+const autoprefixer = require('autoprefixer');
+const tailwind = require('tailwindcss');
+
+module.exports = function(defaults) {
+  let app = new EmberApp(defaults, {
+    postcssOptions: {
+      compile: {
+        plugins: [
+          {
+            module: autoprefixer,
+            options: {}
+          },
+          {
+            module: tailwind,
+            options: {
+              config: './config/tailwindcss-config.js'
+            }
+          }
+        ],
+      }
+    }
+  });
+
+  return app.toTree();
+};
